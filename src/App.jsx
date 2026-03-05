6c45e412d6dc04f8fe6b6740de0de3c35f4f6246
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2026-03-05 07:54:05 +0300

    feat(store-toaster) : install and use zustand to store user and install react-toaster to see message on frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 import "./App.css";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
@@ -7,6 +8,7 @@ import Auth from "./pages/Auth";
 function App() {
   return (
     <ThemeProvider>
+      <Toaster />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
